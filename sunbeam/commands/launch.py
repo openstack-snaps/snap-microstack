f5eb71d8f1d398692de13bc736138b090ce647b1
Jadon Naas <jadon.naas@canonical.com>
2023-04-17 11:30:09 -0400

    Remove unused imports

--- sunbeam/commands/launch.py
+++ sunbeam/commands/launch.py
@@ -13,7 +13,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import asyncio
 import logging
 import os
 import subprocess
@@ -26,8 +25,6 @@ from typing import List
 
 from rich.console import Console
 from snaphelpers import Snap
-
-from sunbeam.commands import juju
 
 LOG = logging.getLogger(__name__)
 console = Console()
